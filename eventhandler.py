63a671b8fa061f4b8ff1999c40859e3cdd22f8a0
Chubakur <chubakur@gmail.com>
2014-01-11 21:02:22 +0400

    Old versions in chaos

--- eventhandler.py
+++ eventhandler.py
@@ -1,19 +1,22 @@
 # -*- coding: utf-8 -*-
 import globals
 import pygame
+import player
 import cards
 import sys
-import os
-import sockets
-import socket
-import menu
 from pygame.locals import *
-current_folder = os.path.dirname(os.path.abspath(__file__))
-
+def play_bookopen_sound():
+    #pygame.mixer.music.load('misc/sounds/book_open.wav')
+    #pygame.mixer.music.play()
+    return
+def play_bookclose_sound():
+    #pygame.mixer.music.load('misc/sounds/book_close.wav')
+    #pygame.mixer.music.play()
+    return
 class Point(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(current_folder+'/misc/point_alpha.gif').convert_alpha()
+        self.image = pygame.image.load('misc/point_alpha.gif').convert_alpha()
         self.rect = self.image.get_rect()
     def draw(self, rect):
         globals.background.blit(self.image, rect)
@@ -21,59 +24,25 @@ class Point(pygame.sprite.Sprite):
         self.rect = self.rect.move(rect)
 class Event_handler():
     def __init__(self):
-        self.onmouse_element = False
+        pass
     def event(self, event):
         if event.type == QUIT:
-            menu.exit_program()
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_ESCAPE:
-                if not globals.question:
-                    if not 'importantmessage' in globals.__dict__:
-                        menu.menu_esc_question()
-                    else: #handle ESC key when display importantmessage
-                        if globals.cli:
-                            if not globals.opponent_disconnect:
-                                sockets.query({"action":"bye","player_id":globals.player_id})
-                            else:
-                                sockets.query({"action":"bbye"})
-                        globals.information_group.remove(globals.importantmessage)
-                        del globals.importantmessage
-                        globals.stage = 0
-                        globals.cli = False
-                    return
-                else:
-                    menu.clean_question()
-                    return
-            if globals.question and (event.key>=K_0 and event.key<=K_9 or event.key>=K_a and event.key<=K_z or event.key==K_PERIOD):
-                globals.answer=globals.answer+pygame.key.name(event.key)
-            if globals.question and (event.key==K_RETURN or len(globals.answer)>=globals.answer_maxchar):
-                exec(globals.answer_cmd)
-                menu.clean_question()
-                return
-            if globals.itemfocus:
-                globals.itemfocus.change(event)
+            sys.exit(0)
         if event.type == MOUSEBUTTONDOWN:
             if event.button == 1:
+                #global player
                 globals.point.draw(event.pos)
-                if globals.stage==0:
-                    collided = pygame.sprite.spritecollide(globals.point, globals.menu_group, 0)
-                elif globals.stage<=2:
-                    collided = pygame.sprite.spritecollide(globals.point, globals.cards_in_deck, 0)
-                    if not collided:
-                        collided = pygame.sprite.spritecollide(globals.point, globals.interface, 0)
-                    if not collided:
-                        collided = pygame.sprite.spritecollide(globals.point, globals.panels, 0)
+                collided = pygame.sprite.spritecollide(globals.point, globals.cards_in_deck, 0)
+                if not collided:
+                    collided = pygame.sprite.spritecollide(globals.point, globals.interface, 0)
+                if not collided:
+                    collided = pygame.sprite.spritecollide(globals.point, globals.panels, 0)
                 if not collided:
                     return
                 item = collided[len(collided)-1]
-                if item.type == 'outer':
-                    return
-                if item.type == 'button' or item.type == 'txtinput' or item.type == 'checkbox':
-                    item.onmousedown()
-                    return
                 if item.type == "warrior_card": #Карта в колоде! Карта на поле в cardbox
-                    #exec('selected_card_0 = cards.' + item.name + '()') #Переменной selected_card_0 присваиваем новый объект
-                    globals.selected_card = cards.links_to_cards[item.name]() # из локальной в глобальную
+                    exec('selected_card_0 = cards.' + item.name + '()') #Переменной selected_card_0 присваиваем новый объект
+                    globals.selected_card = selected_card_0 # из локальной в глобальную
                     if globals.player.id == 1:
                         for cardbox in globals.cardboxes[0:5]:
                             if globals.player.action_points:
@@ -88,19 +57,17 @@ class Event_handler():
                     if not globals.player.action_points: #если уже ходил
                         globals.gameinformationpanel.display("You've already made a move.")
                         return
-                    selected_card = cards.links_to_cards[item.name]()
-                    #exec('selected_card = cards.' + item.name + '()') #в переменную selected_card засовываем одну такую карту
-                    #exec('available_mana = globals.player.' + selected_card.element + '_mana') # Вычисляем сколько маны у нас есть. Значение помещаем в локальную переменную available_mana
-                    available_mana = globals.player.mana[selected_card.element]
+                    exec('selected_card = cards.' + item.name + '()') #в переменную selected_card засовываем одну такую карту
+                    exec('available_mana = globals.player.' + selected_card.element + '_mana') # Вычисляем сколько маны у нас есть. Значение помещаем в локальную переменную available_mana
                     if available_mana >= selected_card.level:
-                        #exec('globals.player.' + selected_card.element + '_mana -= ' + str(selected_card.level)) #Отнимаем ману
-                        globals.player.mana[selected_card.element] -= selected_card.level
+                        exec('globals.player.' + selected_card.element + '_mana -= ' + str(selected_card.level)) #Отнимаем ману
                         globals.player.action_points = False #ставим запись, что ход сделан
                         selected_card.player = globals.player
-                        sockets.query({"action":"card","card":selected_card.name,"type":"magic"})
                         selected_card.cast() #вызываем магию, периодизация делается уже внутри класса, путем добавления в группу globals.magic_cards
-                        selected_card.spell_speaker()
                         #Закрываем колоду
+                        globals.interface.remove(globals.cardsofelementshower1)
+                        globals.interface.remove(globals.cardsofelementshower2)
+                        globals.cards_in_deck.empty()
                         for cardbox in globals.cardboxes:
                             cardbox.light = False
                     else:
@@ -109,18 +76,12 @@ class Event_handler():
                 if globals.cast_focus: #выбор цели для каста
                     if item.type == 'cardbox':
                         globals.cast_focus_wizard.focus_cast_action(item.card)
-                        sockets.query({"action":"cast","position":globals.cast_focus_wizard.parent.position,"target":item.position,"focus":True})
                 if item.player.id != globals.player.id:
                     return
-                if globals.cli:
-                    if item.player.id != globals.player_id:
-                        return
                 if item.type == "cardbox": #Если клик на карточный бокс
                     if item.card.name != "player": #Если в этом блоке есть карта
                         if item.card.cast: #если есть каст
                             if not item.card.used_cast: # если еще не кастовали
-                                if not item.card.focus_cast:
-                                    sockets.query({"action":"cast","position":item.position,"focus":False})
                                 item.card.cast_action()
                             else:
                                 globals.gameinformationpanel.display("You've already cast.")
@@ -143,20 +104,43 @@ class Event_handler():
                             return
                         item.card = globals.selected_card
                         item.card.parent = item
-                        sockets.query({"action":"card","card":item.card.name,"position":item.position,"type":"warrior"})
-                        #item.card.cardboxes = cardboxes
+                        #item.card.cardboxes = card boxes
                         #item.card.playerscards = playerscards
                         item.card.field = True
                         item.card.summon() #функция которая хранит описание действий при выводе карты
+                        item.card.summon_speaker()
                         globals.player.action_points = False
-                        #exec('globals.player.' + globals.selected_card.element + '_mana -= ' + str(globals.selected_card.level)) #Отнимаем ману
-                        globals.player.mana[globals.selected_card.element] -= globals.selected_card.level
-                        #globals.cards_in_deck.empty() #очищаем группу карты в колоде
+                        exec('globals.player.' + globals.selected_card.element + '_mana -= ' + str(globals.selected_card.level)) #Отнимаем ману
+                        globals.interface.remove(globals.cardsofelementshower1) #Закрываем окна выбора карты
+                        globals.interface.remove(globals.cardsofelementshower2) #Закр. окна выбора карты
+                        globals.cards_in_deck.empty() #очищаем группу карты в колоде
                         if item.player.id == 1:
                             globals.ccards_1.add(item.card)
                         else:
                             globals.ccards_2.add(item.card)
                         globals.selected_card = 0
+                if item.type == 'elementbutton':
+#                    global cards_of_element_shower_element
+                    globals.cards_in_deck.empty()
+                    if item.element == 'water':
+                        globals.cards_of_element_shower_element = "water"
+                    elif item.element == 'fire':
+                        globals.cards_of_element_shower_element = "fire"
+                    elif item.element == 'air':
+                        globals.cards_of_element_shower_element = "air"
+                    elif item.element == 'earth':
+                        globals.cards_of_element_shower_element = "earth"
+                    elif item.element == 'life':
+                        globals.cards_of_element_shower_element = "life"
+                    elif item.element == 'death':
+                        globals.cards_of_element_shower_element = "death"
+                    if globals.player.id == 1:
+                        globals.interface.add(globals.cardsofelementshower1)
+                    else:
+                        globals.interface.add(globals.cardsofelementshower2)
+                    play_bookopen_sound()
+                elif item.type == 'completethecoursebutton':
+                    player.finish_turn()
             elif event.button == 3: #ПРАВАЯ КНОПКА МЫШИ
                 if globals.cast_focus:
                     globals.cast_focus = False
@@ -184,6 +168,7 @@ class Event_handler():
                         globals.cardinfo.card = item.card
                         globals.cardinfo.show = True
                 if item.type == 'cardsofelementshower':
+                    play_bookclose_sound()
                     globals.interface.remove(globals.cardsofelementshower1)
                     globals.interface.remove(globals.cardsofelementshower2)
                     globals.cards_in_deck.empty()
@@ -194,25 +179,3 @@ class Event_handler():
                 if globals.cardinfo.show:
                     globals.cardinfo.show = False
                     globals.card_info_group.empty()
-            else: #1
-                for elem in globals.interface:
-                    if elem.type == 'button':
-                        elem.onmouseup()
-        elif event.type == MOUSEMOTION:
-            globals.point.draw(event.pos)
-            if globals.stage==0:
-                collided = pygame.sprite.spritecollide(globals.point, globals.menu_group, 0)
-            elif globals.stage<=2:
-                collided = pygame.sprite.spritecollide(globals.point, globals.interface, 0)
-            if not collided:
-                if self.onmouse_element:
-                    self.onmouse_element.onmouseout()
-                    self.onmouse_element = False
-                return
-            item = collided[len(collided)-1]
-            if item.type == 'button':
-                if self.onmouse_element != item:
-                    item.onmouseout()
-                self.onmouse_element = item
-                item.onmouse()
-#>>>>>>> other
--- player.py
+++ player.py
@@ -1,87 +1,72 @@
 # -*- coding: utf-8 -*-
 #import pygame
 import cards
-import os
 import random
 import globals
-import sockets
-try: 
-    import pygame
-    yes_pygame = True
-except ImportError:
-    yes_pygame = False
-import ai
-current_folder = os.path.dirname(os.path.abspath(__file__))
-
+import pygame
 class Player(): #Прототип игрока
-    def __init__(self, game_id=0):
+    def __init__(self):
         self.health = 50
         self.name = "player"
         self.action_points = True #Ходил игрок, или нет
-        self.game_id = game_id
         self.get_cards()
-        self.mana = {}
         self.get_mana()
-        self.element = "none"
-        self.ai = False
-        self.cards_generated = False
-        self.enemy = None
-    def get_self_cards(self):
-        return globals.ccards_1.sprites() if self.id == 1 else globals.ccards_2.sprites()
     def damage(self, damage, enemy, cast = False):
-        for card in self.get_self_cards():
-            card.owner_gets_damage(damage)
         self.health -= damage
         if self.health <= 0:
             globals.gameinformationpanel.display("Game Over!")
-            globals.stage = False
     def heal(self, health):
         self.health += health
     def get_mana(self):
         #маны должно быть 25 в сумме!!
-        manas = ["water","fire","air","earth","life","death"]
+        manas = ["self.water_mana","self.fire_mana","self.air_mana","self.earth_mana","self.life_mana","self.death_mana"]
         random.shuffle(manas) #раскидываем массив рендомно
         sum = 0
         for mana_id in xrange(0, len(manas)):
             if mana_id != len(manas) - 1:
                 rand = random.randint(2,5)
                 sum+=rand
-                self.mana[manas[mana_id]] = rand
+                exec(manas[mana_id]+"=rand")
             else:
-                self.mana[manas[mana_id]] = 25 - sum
-    def get_mana_count(self): #просмотр кол-ва маны
-        return [self.mana['water'], self.mana['fire'], self.mana['air'], self.mana['earth'], self.mana['life'], self.mana['death']]
-    def get_cards(self, server_cards = None):
-        ''' server_cards = list of cards sent from remote server '''
-        self.cards = {}
-        tmpcards = {}
-        cards_for_sort = {}
-        for element in ['water', 'fire', 'air', 'earth', 'life', 'death']: 
-            tmpcards[element] = {}
-            cards_for_sort[element] = []
-            for i in xrange(0, 4):
-                #получаем карту элемента воды
-                if not server_cards: 
-                    randnum = random.randint(0, len(globals.games_cards[self.game_id][element])-1)
-                    card = globals.games_cards[self.game_id][element][randnum]
-                    globals.games_cards[self.game_id][element].remove(card)
-                else:
-                    card = server_cards[element][i]
-                if self.game_id == 0: 
-                    tmpcards[element][card] = cards.links_to_cards[card]()
-                    cards_for_sort[element].append([tmpcards[element][card].level, tmpcards[element][card]])
-                else: 
-                    tmpcards[element][card] = card
-
-            if self.game_id == 0: 
-                cards_for_sort[element].sort()
-                for i in xrange(0,4):
-                    cards_for_sort[element][i][1].position_in_deck = i
-        self.cards = tmpcards.copy()
-
-        del cards_for_sort
-        del tmpcards
-
+                exec(manas[mana_id]+"=25-sum")
+    def get_cards(self):
+        self.water_cards = []
+        self.fire_cards = []
+        self.air_cards = []
+        self.earth_cards = []
+        self.life_cards = []
+        self.death_cards = []
+        for i in xrange(0, 4):
+            #получаем карту элемента воды
+            randnum = random.randint(0, len(cards.water_cards)-1)
+            self.water_cards.append(cards.water_cards[randnum])
+            exec("self." + cards.water_cards[randnum].lower() + "=cards." + cards.water_cards[randnum] + "()")
+            cards.water_cards.remove(cards.water_cards[randnum])
+            #Элемент огня
+            randnum = random.randint(0, len(cards.fire_cards)-1)
+            self.fire_cards.append(cards.fire_cards[randnum])
+            exec("self." + cards.fire_cards[randnum].lower() + "=cards." + cards.fire_cards[randnum] + "()")
+            cards.fire_cards.remove(cards.fire_cards[randnum])
+            #Элемент воздуха
+            randnum = random.randint(0, len(cards.air_cards)-1)
+            self.air_cards.append(cards.air_cards[randnum])
+            exec("self." + cards.air_cards[randnum].lower() + "=cards." + cards.air_cards[randnum] + "()")
+            cards.air_cards.remove(cards.air_cards[randnum])
+            #Элемент земли
+            randnum = random.randint(0, len(cards.earth_cards)-1)
+            self.earth_cards.append(cards.earth_cards[randnum])
+            exec("self." + cards.earth_cards[randnum].lower() + "=cards." + cards.earth_cards[randnum] + "()")
+            cards.earth_cards.remove(cards.earth_cards[randnum])
+            #Элемент жизни
+            randnum = random.randint(0, len(cards.life_cards)-1)
+            self.life_cards.append(cards.life_cards[randnum])
+            exec("self." + cards.life_cards[randnum].lower() + "=cards." + cards.life_cards[randnum] + "()")
+            cards.life_cards.remove(cards.life_cards[randnum])
+            #Элемент смерти
+            randnum = random.randint(0, len(cards.death_cards)-1)
+            self.death_cards.append(cards.death_cards[randnum])
+            exec("self." + cards.death_cards[randnum].lower() + "=cards." + cards.death_cards[randnum] + "()")
+            cards.death_cards.remove(cards.death_cards[randnum])
 class Player1(Player):
     def __init__(self):
         self.id = 1
@@ -90,76 +75,40 @@ class Player2(Player):
     def __init__(self):
         self.id = 2
         Player.__init__(self)
-def switch_position():
-    #globals.attack_started = False
-    globals.attack_started.pop()
-    n = globals.nickname1.name
-    globals.nickname1.set_nickname(globals.nickname2.name)
-    globals.nickname2.set_nickname(n)
-    for cardbox in globals.cardboxes:
-        cardbox.opposite = not cardbox.opposite
-def me_finish_turn():
+def finish_turn():
     #Добавляем ману другому игроку.
-    #globals.attack_started = True
-    globals.attack_started.append(True)
-    globals.player.enemy.mana['water'] += 1
-    globals.player.enemy.mana['fire'] += 1
-    globals.player.enemy.mana['air'] += 1
-    globals.player.enemy.mana['earth'] += 1
-    globals.player.enemy.mana['life'] += 1
-    globals.player.enemy.mana['death'] += 1
+    globals.player.enemy.water_mana += 1
+    globals.player.enemy.fire_mana += 1
+    globals.player.enemy.air_mana += 1
+    globals.player.enemy.earth_mana += 1
+    globals.player.enemy.life_mana += 1
+    globals.player.enemy.death_mana += 1
     #Меняем игрока
-    try:
-        pygame.mixer.music.load(current_folder+'/misc/sounds/card_attack.mp3')
-    except:
-        print "Unexpected error: while trying play attack sound"
-    globals.playmusic()
     if globals.player.id == 1:
         globals.player = globals.player2
         globals.player.action_points = True
+        for spell in globals.magic_cards: #вызываем функцию повторения магия
+            spell.periodical_cast()
         for card in globals.ccards_1: #Атакуем
             kill = card.attack()
             if kill:
                 card.enemy_die()
             card.used_cast = False
-        for spell in globals.magic_cards: #вызываем функцию повторения магия
-            spell.periodical_cast()
         for card in globals.ccards_2:
             card.turn()
-            card.moves_alive += 1
         for card in globals.ccards_2:
             card.additional_turn_action()
     else:
         globals.player = globals.player1
         globals.player.action_points = True
+        for spell in globals.magic_cards: #вызываем функцию повторения магия
+            spell.periodical_cast()
         for card in globals.ccards_2: #Атакуем
             kill = card.attack()
             if kill:
                 card.enemy_die()
             card.used_cast = False
-        for spell in globals.magic_cards: #вызываем функцию повторения магия
-            spell.periodical_cast()
         for card in globals.ccards_1:
             card.turn()
-            card.moves_alive += 1
         for card in globals.ccards_1:
             card.additional_turn_action()
-    if globals.player.ai:
-        cb = ai.select_cardbox()
-        if cb:
-            c = ai.select_card(cb.card)
-            #print 'SELECTED',c
-            cb.card = c()
-            cb.card.field = True
-            globals.player.mana[cb.card.element] -= cb.card.level
-            cb.card.parent = cb
-            if globals.player.id == 1:
-                globals.ccards_1.add(cb.card)
-            else:
-                globals.ccards_2.add(cb.card)
-            cb.card.summon()
-        finish_turn()
-def finish_turn():
-    me_finish_turn()
-    sockets.query({"action":"switch_turn"})
-    #switch_position()
