eac46f84082f192df0599434b6192b15c0971d66
Andrey Pitko <chubakur@gmail.com>
2015-01-17 19:35:02 +0300

    2-nd patch by Sandro Bonazzola

--- setup.py
+++ setup.py
@@ -1,41 +1,77 @@
-__author__="chubakur"
-__date__ ="$08.04.2011 19:30:49$"
-#Wizards Magic
-#Copyright (C) 2011-2014  https://code.google.com/p/wizards-magic/
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+#!/usr/bin/python -O
+# -*- coding: utf-8 -*-
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# Wizards Magic
+# Copyright (C) 2011-2014  https://code.google.com/p/wizards-magic/
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-from setuptools import setup,find_packages
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-setup (
-  name = 'WizardsMagic',
-  version = '0.1',
-  packages = find_packages(),
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-  # Declare your packages' dependencies here, for eg:
-  install_requires=['foo>=3'],
+from setuptools import setup
+from glob import glob
+import py_compile
+import os
 
-  # Fill in these to make your Egg ready for upload to
-  # PyPI
-  author = 'chubakur',
-  author_email = '',
+modules = [
+    x
+    for x in glob('*.py')
+    if x != 'setup.py'
+]
+print modules
+for f in modules:
+    os.environ['PYTHONOPTIMIZE'] = "0"
+    py_compile.compile(f, f+'c')
+    os.environ['PYTHONOPTIMIZE'] = "1"
+    py_compile.compile(f, f+'o')
 
-  summary = 'Just another Python package for the cheese shop',
-  url = '',
-  license = '',
-  long_description= 'Long description of the package',
+data_files=[
+    ('', modules + [f+'c' for f in modules] + [f+'o' for f in modules])
+]
 
-  # could also include long_description, download_url, classifiers, etc.
+for d in ('misc', 'languages'):
+    for root, dirs, files in os.walk(d):
+        if files:
+            data_files.append((root, [os.path.join(root, f) for f in files]))
 
-  
+print data_files
+
+setup(
+    name='WizardsMagic',
+    version='3.2.21',
+    license='GPLv2+',
+    description='Wizards Magic - OpenSource Card Game',
+    url='https://code.google.com/p/wizards-magic/',
+    author='Andrey Pitko',
+    author_email='chubakur@gmail.com',
+
+    long_description=(
+        'Wizards Magic - OpenSource card game, based on rules of '
+        'Magic: The Gathering, written in Python, using Pygame'
+    ),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: X11 Applications',
+        'Intended Audience :: End Users/Desktop',
+        (
+            'License :: OSI Approved :: '
+            'GNU General Public License v2 or later (GPLv2+)'
+        ),
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Games/Entertainment :: Turn Based Strategy'
+    ],
+    install_requires=['simplejson', 'pygame'],
+    requires=['simplejson', 'pygame'],
+    data_files=data_files,
+    scripts=['WizardsMagic'],
 )
