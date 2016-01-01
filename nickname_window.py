d78ab538bea450f81e3691d08f5d68a31d45b963
Sandro Bonazzola <sandro.bonazzola@gmail.com>
2016-01-01 11:12:44 +0100

    pep8: nickname_window.py
    Fixed pep8 issues without changing any logic

--- nickname_window.py
+++ nickname_window.py
@@ -1,37 +1,42 @@
-#Wizards Magic
-#Copyright (C) 2011-2014  https://code.google.com/p/wizards-magic/
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+# Wizards Magic
+# Copyright (C) 2011-2014  https://code.google.com/p/wizards-magic/
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 import wzglobals
 import pygame
-#TODO: cout as wzglobals.player
+# TODO: cout as wzglobals.player
+
+
 class NicknameWindow(pygame.sprite.Sprite):
-    def __init__(self,rect, nickname):
+    def __init__(self, rect, nickname):
         pygame.sprite.Sprite.__init__(self)
         self.type = 'nicknamewindow'
-        #self.rect = rect
+        # self.rect = rect
         self.name = nickname
-        self.nickname = wzglobals.font2.render(self.name, True, (255,255,255))
+        self.nickname = \
+            wzglobals.font2.render(self.name, True, (255, 255, 255))
         self.rect = self.nickname.get_rect()
         self.rect = self.rect.move(rect)
         wzglobals.interface.add(self)
+
     def draw(self):
-        #return
+        # return
         wzglobals.background.blit(self.nickname, self.rect)
+
     def set_nickname(self, nickname):
         self.name = nickname
-        self.nickname = wzglobals.font2.render(nickname, True, (255,255,255))
+        self.nickname = wzglobals.font2.render(nickname, True, (255, 255, 255))
+
     def update(self):
         self.draw()
-
